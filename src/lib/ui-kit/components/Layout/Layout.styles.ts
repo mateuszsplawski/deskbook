234cdaa5ef3075bab90c1941b2f2e426b7621178
Mateusz Spławski <mateusz.splawski@netguru.com>
2021-12-31 11:26:10 +0100

    feat: Add Layout component composition

---
+++ src/lib/ui-kit/components/Layout/Layout.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledLayoutWrapper = styled.div``;
+
+export const StyledNavigation = styled.nav``;
+
+export const StyledFooter = styled.footer``;
+
+export const StyledMain = styled.main``;
