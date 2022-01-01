1adad24d556b46be61345b4d7b6cb0f0f2fc408f
Mateusz Spławski <mateusz.splawski@netguru.com>
2022-01-01 18:34:06 +0100

    feat: Add Seo component

--- src/lib/ui-kit/components/Layout/Layout.component.tsx
+++ src/lib/ui-kit/components/Layout/Layout.component.tsx
@@ -1,26 +1,40 @@
 import { ReactNode } from "react";
 import { Logo } from "../Logo/Logo.component";
 import { Navigation } from "./components/Navigation/Navigation.component";
+import { Seo } from "./components/Seo/Seo.component";
+import { SeoMetadata } from "./components/Seo/Seo.types";
 import {
   StyledFooter as Footer,
   StyledLayoutWrapper as Wrapper,
   StyledMain as Main,
 } from "./Layout.styles";
 
-interface LayoutProps {
+interface LayoutProps extends SeoMetadata {
   children: ReactNode;
 }
 
-export const Layout = ({ children }: LayoutProps) => {
+export const Layout = ({
+  children,
+  metaDescription,
+  metaTitle,
+  metaRobots,
+}: LayoutProps) => {
   return (
-    <Wrapper>
-      <Navigation
-        leftSide={<Logo />}
-        center={<p>Center</p>}
-        rightSide={<p>Right</p>}
+    <>
+      <Seo
+        metaTitle={metaTitle}
+        metaDescription={metaDescription}
+        metaRobots={metaRobots}
       />
-      <Main>{children}</Main>
-      <Footer>Footer</Footer>
-    </Wrapper>
+      <Wrapper>
+        <Navigation
+          leftSide={<Logo />}
+          center={<p>Center</p>}
+          rightSide={<p>Right</p>}
+        />
+        <Main>{children}</Main>
+        <Footer>Footer</Footer>
+      </Wrapper>
+    </>
   );
 };
