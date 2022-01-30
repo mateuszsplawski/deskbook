ed5b4fe4e743151c268b2ac03a8cd4563be72bb9
Mateusz Spławski <mateusz.splawski@netguru.com>
2022-01-30 17:17:20 +0100

    feat: Add register form, validation and translations

---
+++ src/app/pages/Register/Register.styles.ts
@@ -0,0 +1,37 @@
+import { Form } from "formik";
+import { MEDIA_QUERIES } from "lib/ui-kit";
+import styled from "styled-components";
+
+export const StyledDeskWithMacbookImage = styled.img`
+  display: none;
+
+  ${MEDIA_QUERIES.tablet} {
+    display: block;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const StyledLeftColumn = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledRegisterForm = styled(Form)`
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+
+  ${MEDIA_QUERIES.tablet} {
+    display: flex;
+    > * {
+      max-width: 35rem;
+    }
+  }
+`;
--- src/lib/form-kit/index.ts
+++ src/lib/form-kit/index.ts
@@ -1,2 +1,4 @@
+export { registerValidationSchema } from "./fieldsets/Register/registerValidationSchema";
 export { loginValidationSchema } from "./fieldsets/Login/loginValidationSchema";
+export { RegisterFieldset } from "./fieldsets/Register/Register.fieldset";
 export { LoginFieldset } from "./fieldsets/Login/Login.fieldset";
