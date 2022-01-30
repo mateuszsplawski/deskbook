147d4961c8b5825e43e6aed0a4a6957c3cbcf553
Mateusz Spławski <mateusz.splawski@netguru.com>
2022-01-30 17:44:26 +0100

    feat: Add reset password form, validation and translations

--- src/lib/form-kit/index.ts
+++ src/lib/form-kit/index.ts
@@ -1,4 +1,6 @@
+export { resetPasswordValidationSchema } from "./fieldsets/ResetPassword/resetPasswordValidationSchema";
 export { registerValidationSchema } from "./fieldsets/Register/registerValidationSchema";
 export { loginValidationSchema } from "./fieldsets/Login/loginValidationSchema";
+export { ResetPasswordFieldset } from "./fieldsets/ResetPassword/ResetPassword.fieldset";
 export { RegisterFieldset } from "./fieldsets/Register/Register.fieldset";
 export { LoginFieldset } from "./fieldsets/Login/Login.fieldset";
